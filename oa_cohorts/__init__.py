cbbaa8f9f0e48eb3b9e3515d400ebf0883ff9fab
georgie <georgina.kennedy@unsw.edu.au>
2024-03-01 16:08:17 +1100

    exposing enums

--- oa_cohorts/__init__.py
+++ oa_cohorts/__init__.py
@@ -1,3 +1,3 @@
-from .cohorts import Dash_Cohort, Dash_Cohort_Def, Dash_Cohort_Rule, Dash_Cohort_Dx, Dash_Cohort_Dx_Rule, Dash_Cohort_Tx_Rule
+from .cohorts import Dash_Cohort, Dash_Cohort_Def, Dash_Cohort_Rule, Dash_Cohort_Dx, Dash_Cohort_Dx_Rule, Dash_Cohort_Tx_Rule, RuleCombination, RuleType, DxType, TxType
 
-__all__ = [Dash_Cohort, Dash_Cohort_Def, Dash_Cohort_Rule, Dash_Cohort_Dx, Dash_Cohort_Dx_Rule, Dash_Cohort_Tx_Rule]
+__all__ = [Dash_Cohort, Dash_Cohort_Def, Dash_Cohort_Rule, Dash_Cohort_Dx, Dash_Cohort_Dx_Rule, Dash_Cohort_Tx_Rule,  RuleCombination, RuleType, DxType, TxType]
