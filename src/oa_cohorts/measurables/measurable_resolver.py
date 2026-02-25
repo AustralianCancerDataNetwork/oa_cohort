bd752adf37dd90e68345ddc92b957fc9c967d214
gkennos <georgina.kennedy@unsw.edu.au>
2026-02-25 22:03:22 +1100

    pre-merge

--- src/oa_cohorts/measurables/measurable_resolver.py
+++ src/oa_cohorts/measurables/measurable_resolver.py
@@ -19,3 +19,26 @@ def get_measurable_registry() -> dict[RuleTarget, Type[MeasurableBase]]:
         # RuleTarget.proc_concept: ProcedureMeasurable,
         # ...
     }
+
+
+#             target             | count 
+# -------------------------------+-------
+#  referral_to_specialist_window |     1
+#  intent_rt                     |     1
+#  tx_to_death_window            |     1
+
+#  proc_concept                  |     4
+#  meas_concept                  |     9
+#  obs_concept                   |     2
+
+#  demog_gender                  |     3
+#  demog_death                   |     1
+
+#  tx_concurrent                 |     1
+#  tx_chemotherapy               |     1
+#  tx_surgical                   |     3
+#  tx_current_episode            |     2
+
+#  dx_stage                      |    77
+#  dx_mets                       |     2
+#  dx_primary                    |    39
