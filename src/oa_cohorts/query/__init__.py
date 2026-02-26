2d4cd8c460da1cf2856b4ef62efd63cf6443366e
gkennos <georgina.kennedy@unsw.edu.au>
2026-02-26 14:33:05 +1100

    string matcher attributes

--- src/oa_cohorts/query/__init__.py
+++ src/oa_cohorts/query/__init__.py
@@ -2,7 +2,7 @@ from .indicator import Indicator, report_indicator_map
 from .measure import Measure, MeasureRelationship
 from .query_plan import MeasureNode, SubqueryNode, QueryNode, QueryPlan
 from .report import Report, ReportCohortMap, ReportVersion
-from .subquery import Subquery
+from .subquery import Subquery, subquery_rule_map
 from .query_rule import QueryRule, ExactRule, HierarchyExclusionRule, HierarchyRule, AbsenceRule, ScalarRule, PhenotypeRule, SubstringRule
 from .phenotype import Phenotype, PhenotypeDefinition
 from .dash_cohort import DashCohort, DashCohortDef, dash_cohort_def_map
@@ -22,6 +22,7 @@ __all__ = [
     "Report",
     "ReportCohortMap",
     "Subquery",
+    "subquery_rule_map",
     "QueryRule",
     "ExactRule",
     "HierarchyExclusionRule",
